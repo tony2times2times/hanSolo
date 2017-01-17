45bcec9b4988fbcbaebcadde1e3b7c4b24704391
tony2times2times <tony.peraza86@gmail.com>
2017-01-17 17:50:18 -0600

    search is working and movies are diplaying netflix results

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,9 +11,8 @@ togetherApp.config(["$routeProvider", function($routeProvider) {
         .otherwise({
             redirectTo: "/home"
         });
-
-
 }]);
+
 togetherApp.factory("flix", function() {
     var flix = {
         found: []
@@ -22,55 +21,86 @@ togetherApp.factory("flix", function() {
     return flix;
 });
 
-togetherApp.controller('homeController', ["$scope", "$http", "flix", function($scope, $http, flix) {
-    console.log("Home Controller loaded.");
-    $scope.flix = [];
+togetherApp.controller('homeController', ["$scope", "$http", "flix",
+    function($scope, $http, flix) {
+        console.log("Home Controller loaded.");
+        $scope.flix = [];
 
+        $scope.netflixSearch = function(title, year, index) {
+            $http({
+                method: 'GET',
+                url: ('http://netflixroulette.net/api/api.php?title=' +
+                    title + '&year=' + year)
+            }).then(function successCallback(response) {
+                console.log(response);
+                $scope.flix[index].netflix = true;
+            }, function errorCallback(error) {
+                //console.log('error', error);
+            });
+        };
+        $scope.theMovieDBSearch = function(title) {
+            $http({
+                method: 'GET',
+                url: '/theMovieDB/' + title
+            }).then(function successCallback(response) {
+                console.log(response);
+                //saves found movies to the factory
+                $scope.flix = response.data.results;
+                $scope.getInfo();
+            }, function errorCallback(error) {
+                console.log('error', error);
+            });
+        };
 
-    $scope.neflixSearch = function(title) {
-        console.log(title);
-        $http({
-            method: 'GET',
-            url: 'http://netflixroulette.net/api/api.php?title=Attack%20on%20titan'
-        }).then(function successCallback(response) {
-            console.log(response);
-        }, function errorCallback(error) {
-            console.log('error', error);
-        });
-    };
-    $scope.theMovieDBSearch = function(title) {
-        $http({
-            method: 'GET',
-            url: '/theMovieDB/' + title
-        }).then(function successCallback(response) {
-            console.log(response);
-            flix.found = response.data.results;
-            $scope.flix = flix.found;
-            $scope.poster();
-        }, function errorCallback(error) {
-            console.log('error', error);
-        });
-    };
+        //search for a title
+        $scope.search = function() {
+            //pull title off the DOM
+            var title = $scope.title;
+            console.log('User entered: ' + title);
+            //url encode the title and remove leading and trailing
+            //white space
+            title = encodeURIComponent(title.trim());
+            console.log('encoded to: ' + title);
+            $scope.theMovieDBSearch(title);
+        };
 
-    $scope.search = function() {
-        //pull title off the DOM
-        var title = $scope.title;
-        console.log('User entered: ' + title);
-        //url encode the title and remove leading and trailing white space
-        title = encodeURIComponent(title.trim());
-        console.log('encoded to: ' + title);
-        $scope.theMovieDBSearch(title);
-    };
-    $scope.poster = function() {
-        for (var i = 0; i < $scope.flix.length; i++) {
-            if ($scope.flix[i].poster_path === null) {
-              $scope.flix[i].poster = '../images/black.jpg';
-            } else {
-                $scope.flix[i].poster = 'https://image.tmdb.org/t/p/w500' + $scope.flix[i].poster_path;
+        //processes the flix completing the poster url and thier
+        //steaming info and display properties
+        $scope.getInfo = function() {
+            for (var i = 0; i < $scope.flix.length; i++) {
+                $scope.flix[i].netflix = false;
+                $scope.flix[i].viewPoster = true;
+                $scope.flix[i].info = false;
+                var title = $scope.flix[i].title;
+                var year = ($scope.flix[i].release_date.split(/-/))[0];
+                $scope.netflixSearch(title, year, i);
+                if ($scope.flix[i].poster_path === null) {
+                    $scope.flix[i].poster = '../images/black.jpg';
+                } else {
+                    $scope.flix[i].poster = ('https://image.tmdb.org/t/p/w500' +
+                        $scope.flix[i].poster_path);
+                }
             }
-        }
-    };
-}]);
+        };
 
+        $scope.showInfo = function(index) {
+            for (var i = 0; i < $scope.flix.length; i++) {
+                $scope.flix[i].viewPoster = true;
+                $scope.flix[i].info = false;
+            }
+            console.log('changeing view for index: ' + index);
+            $scope.flix[index].viewPoster = false;
+            $scope.flix[index].info = true;
+        };
+    }
+]);
+
+//     setTimeout(function() {
+//     console.log('first function finished');
+//     //angular is stupid and does nto refresh the DOM after the delay...stupid
+//     $scope.$apply();
+// }, 1000);
+
+//link to netflix https://www.netflix.com/title/ + id
 //https://api.themoviedb.org/3/search/movie?api_key=661fc8b62286cda55f62d1ec5979c828&query=
 //Posters https://image.tmdb.org/t/p/w300_and_h450_bestv2/POSTERURL
