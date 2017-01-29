61904196bc4d797566e97f6371f54a88557072bb
tony2times2times <tony.peraza86@gmail.com>
2017-01-28 22:33:54 -0600

    captcha verification required to send

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -272,22 +272,59 @@ function($scope, $http, flix) {
   window.onSignIn = onSignIn;
 }]);
 
-togetherApp.controller('selectedController', ["$scope", 'vcRecaptchaService', "$http", "flix",
-function($scope, vcRecaptchaService, $http, flix) {
-  console.log('selectedController standing by.');
+togetherApp.controller('selectedController', ["$scope", 'vcRecaptchaService',
+"$http", "flix", function($scope, vcRecaptchaService, $http, flix) {
+  console.log('$selectedController standing by.');
   $scope.selectedFlick = flix.selectedFlick;
+  $scope.humanValidation = null;
+  $scope.widgetId = null;
+  $scope.model = {key: '6Ld4gxMUAAAAAASHnclfSWBIZK0ZdLCxorB4Y_vr'};
 
-  $scope.watchTogetherEmail = function() {
-    var movieNight = {};
-    movieNight.partner = $scope.watchWith;
-    movieNight.flick = $scope.selectedFlick;
-    $http({
-      method: 'PUT',
-      url: '/auth/watchTogether',
-      data: movieNight
-    }).then(function successCallback(response) {
-      console.log(response);
-      swal("Email Sent", "", "success");
-    }, function errorCallback(error) {
-      console.log('error', error);
-    });};}]);
+  $scope.setResponse = function (response) {
+    console.info('Response available');
+    $scope.humanValidation = response;
+  };
+  $scope.setWidgetId = function (widgetId) {
+    console.info('Created widget ID: %s', widgetId);
+    $scope.widgetId = widgetId;
+  };
+  $scope.cbExpiration = function() {
+    console.info('Captcha expired. Resetting response object');
+    vcRecaptchaService.reload($scope.widgetId);
+    $scope.humanValidation = null;
+  };
+  $scope.submit = function () {
+    var valid;
+    console.log('sending the captcha response to the server', $scope.humanValidation);
+    if (valid) {console.log('Success');
+  } else {
+    console.log('Failed validation');
+    // In case of a failed validation you need to reload the captcha
+    // because each response can be checked just once
+    vcRecaptchaService.reload($scope.widgetId);
+  }
+};
+$scope.watchTogetherEmail = function() {
+  if ($scope.humanValidation === null) {
+    swal('Please confirm you are not a robot' ,
+    '01101000 01110101 01101101 01100001 01101110 ', 'error');
+  }else{
+  console.log('sending the captcha response to the server', $scope.humanValidation);
+  var movieNight = {};
+  movieNight.partner = $scope.watchWith;
+  movieNight.flick = $scope.selectedFlick;
+  movieNight.humanValidation = $scope.humanValidation;
+  $http({
+    method: 'PUT',
+    url: '/auth/watchTogether',
+    data: movieNight
+  }).then(function successCallback(response) {
+    console.log(response);
+    swal("Email Sent", "", "success");
+  }, function errorCallback(error) {
+    console.log('error', error);
+  }
+);
+}
+};
+}]);
